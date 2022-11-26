33852b9bd97e3f8263045a99f118872a9be5950c
MaxRegner <59399856+MaxRegner@users.noreply.github.com>
2022-11-26 16:28:56 +0100

    Update boot1.s

--- CloverEFI/BootSector/boot1.s
+++ CloverEFI/BootSector/boot1.s
@@ -1,4 +1,4 @@
-; Copyright (c) 1999-2003 Apple Computer, Inc. All rights reserved.
+Copyright (c) 1999-2003 MR , Inc. All rights reserved.
 ;
 ; @APPLE_LICENSE_HEADER_START@
 ; 
@@ -36,7 +36,7 @@
 ;
 ; NOTE: this is an experimental version with multiple extent support.
 ;
-; Written by Tamás Kosárszky on 2008-04-14
+; Written by Tamï¿½s Kosï¿½rszky on 2008-04-14
 ;
 
 ;
@@ -55,30 +55,26 @@ UNUSED				EQU		0
 VERBOSE				EQU		0
 
 ;
-; Various constants.
-;
-NULL		   		EQU		0
-CR					EQU		0x0D
-LF					EQU		0x0A
+; Set to 1 to enable debug mode.
 
-mallocStart			EQU		0x1000								; start address of local workspace area
-maxSectorCount		EQU		64									; maximum sector count for readSectors
-maxNodeSize			EQU		16384
 
-kSectorBytes		EQU		512									; sector size in bytes
-kBootSignature		EQU		0xAA55								; boot sector signature
+DEBUG_MODE			EQU		0
 
-kBoot1StackAddress	EQU		0xFFF0								; boot1 stack pointer
-kBoot1LoadAddr		EQU		0x7C00								; boot1 load address
-kBoot1RelocAddr		EQU		0xE000								; boot1 relocated address
-kBoot1Sector1Addr	EQU		kBoot1RelocAddr + kSectorBytes		; boot1 load address for sector 1
-kHFSPlusBuffer		EQU		kBoot1Sector1Addr + kSectorBytes	; HFS+ Volume Header address
-
-kBoot2Sectors		EQU		(480 * 1024 - 512) / kSectorBytes	; max size of 'boot' file in sectors = 448 but I want 472
-kBoot2Segment		EQU		0x2000								; boot2 load segment
-kBoot2Address		EQU		kSectorBytes						; boot2 load address
 
 ;
+; Set to 1 to enable debug mode for the boot file search.
+;
+DEBUG_SEARCH			EQU		0
+
+;
+; Set to 1 to enable debug mode for the boot file loading.
+;
+DEBUG_LOAD			EQU		0
+
+;
+; Set to 1 to enable debug mode for the boot file execution.
+
+
 ; Format of fdisk partition entry.
 ;
 ; The symbol 'part_size' is automatically defined as an `EQU'
@@ -785,696 +781,61 @@ ConvertStrToUni:
 
 %endif ; UNUSED
 
+%if DEBUG
+
 ;--------------------------------------------------------------------------
-; Convert big-endian HFSUniStr255 to little-endian
+; Print a 32-bit value in hex.
 ;
 ; Arguments:
-;   DS:SI = pointer to big-endian HFSUniStr255
-;	ES:DI = pointer to result buffer
+;   EAX = Value to be displayed in hex.
 ;
-ConvertHFSUniStr255ToLE:
+PrintHex:
 	pushad
-	lodsw
-	xchg	ah, al
-	stosw
-	cmp		al, 0
-	je		.exit
-	mov		cx, ax
-
+	mov     cx, WORD 4
+	bswap   eax
 .loop:
-	lodsw
-	xchg	ah, al							; convert AX to little-endian
+	push    ax
+	ror     al, 4
+	call    PrintNibble            		; display upper nibble
+	pop     ax
+	call    PrintNibble            		; display lower nibble
+	ror     eax, 8
+	loop    .loop
 
-	;
-	; When working with a case-sensitive HFS+ (HX) filesystem, we shouldn't change the case.
-	;
-	cmp		BYTE [kHFSPlusBuffer + HFSPlusVolumeHeader.signature + 1], kHFSPlusCaseSigX
-	je		.keepcase
-
-	or		ax, ax
-	jne		.convertToLE
-	dec		ax								; NULL must be the strongest char
-
-.convertToLE:
-	cmp		ah, 0
-	ja		.keepcase
-	cmp		al, 'A'
-	jb		.keepcase
-	cmp		al, 'Z'
-	ja		.keepcase
-	add		al, 32							; convert to lower-case
-
-.keepcase:
-	stosw
-	loop	.loop
-
-.exit:
-	popad
-	ret
-
-;--------------------------------------------------------------------------
-; compare HFSPlusExtentKey structures
-;
-; Arguments:
-;   DS:SI = search key
-;   ES:DI = trial key
-;
-; Returns:
-;	[BTree.searchResult] = result
-;	FLAGS = relation between search and trial keys
-;
-compareHFSPlusExtentKeys:
-	pushad
-	
-	mov		dl, 0							; DL = result of comparison, DH = bestGuess
-	mov		eax, [si + HFSPlusExtentKey.fileID]
-	cmp		eax, [di + HFSPlusExtentKey.fileID]
-	jne		.checkFlags
-	
-	cmp		BYTE [si + HFSPlusExtentKey.forkType], kForkTypeData
-	jne		.checkFlags
-	
-	mov		eax, [si + HFSPlusExtentKey.startBlock]
-	cmp		eax, [di + HFSPlusExtentKey.startBlock]
-	je		compareHFSPlusCatalogKeys.exit
-
-.checkFlags:
-	ja		compareHFSPlusCatalogKeys.searchKeyGreater		; search key > trial key
-	jb		compareHFSPlusCatalogKeys.trialKeyGreater		; search key < trial key
-
-;--------------------------------------------------------------------------
-; Compare HFSPlusCatalogKey structures
-;
-; Arguments:
-;   DS:SI = search key
-;   ES:DI = trial key
-;
-; Returns:
-;	[BTree.searchResult] = result
-;	FLAGS = relation between search and trial keys
-;
-compareHFSPlusCatalogKeys:
-	pushad
-	xor		dx, dx					; DL = result of comparison, DH = bestGuess
-	xchg	si, di
-	lodsd
-	mov		ecx, eax				; ECX = trial parentID
-	xchg	si, di
-	lodsd							; EAX = search parentID
-	cmp		eax, ecx
-	ja		.searchKeyGreater		; search parentID > trial parentID
-	jb		.trialKeyGreater		; search parentID < trial parentID
-
-.compareNodeName:					; search parentID = trial parentID
-	xchg	si, di
-    lodsw
-    mov		cx, ax					; CX = trial nodeName.length
-	xchg	si, di
-    lodsw							; AX = search nodeName.length
-	cmp		cl, 0					; trial nodeName.length = 0?
-	je		.searchKeyGreater
-
-    cmp		ax, cx
-	je		.strCompare
-    ja		.searchStrLonger
-
-.trialStrLonger:
-    dec		dh
-    mov		cx, ax
-    jmp		.strCompare
-
-.searchStrLonger:
-    inc		dh
-
-.strCompare:
-    repe	cmpsw
-    ja		.searchKeyGreater
-    jb		.trialKeyGreater
-    mov		dl, dh
-    jmp		.exit
-
-.trialKeyGreater:
-    dec		dl
-    jmp		.exit
-    
-.searchKeyGreater:
-    inc		dl
-	
-.exit:
-	mov		[bp + BTree.searchResult], dl
-    cmp		dl, 0							; set flags to check relation between keys
+	mov     al, 10							; carriage return
+	call    PrintChar
+	mov     al, 13
+	call    PrintChar
 
 	popad
 	ret
 
-;--------------------------------------------------------------------------
-; Allocate memory
-;
-; Arguments:
-;   CX = size of requested memory
-;
-; Returns:
-;   BP = start address of allocated memory
-;
-; Clobber list:
-;   CX
-;
-malloc:
-	push	ax								; save AX
-	push	di								; save DI
-	mov		di, [gMallocPtr]				; start address of free space
-	push	di								; save free space start address
-	inc		di								;
-	inc		di								; keep the first word untouched
-	dec		cx								; for the last memory block pointer.
-	dec		cx								;
-	mov		al, NULL						; fill with zero
-	rep		stosb							; repeat fill
-	mov		[gMallocPtr], di				; adjust free space pointer
-	pop		bp								; BP = start address of allocated memory
-	mov		[di], bp						; set start address of allocated memory at next
-											; allocation block's free space address.
-	pop		di								; restore DI
-	pop		ax								; restore AX
-	ret
-
-%if UNUSED
-
-;--------------------------------------------------------------------------
-; Free allocated memory
-;
-; Returns:
-;   BP = start address of previously allocated memory
-;
-free:
-	lea		bp, [gMallocPtr]
-	mov		bp, [bp]
-	mov		[gMallocPtr], bp
-	ret
-
-%endif ; UNUSED
-
-;--------------------------------------------------------------------------
-; Static data.
-;
-
-%if VERBOSE
-root_str			db		'/boot', NULL
-%endif
-
-;--------------------------------------------------------------------------
-; Pad the rest of the 512 byte sized sector with zeroes. The last
-; two bytes is the mandatory boot sector signature.
-;
-; If the booter code becomes too large, then nasm will complain
-; that the 'times' argument is negative.
-
-pad_table_and_sig:
-	times			510-($-$$) db 0
-	dw				kBootSignature
-
-;
-; Sector 1 code area
-;
-
-;--------------------------------------------------------------------------
-; lookUpBTree - initializes a new BTree instance and
-;               look up for HFSPlus Catalog File or Extent Overflow keys
-;
-; Arguments:
-;   AL = kHFSPlusFileID (Catalog or Extents Overflow)
-;	SI = address of searchKey
-;   DI = address of HFSPlusForkData.extents
-;
-; Returns:
-;   BP = address of BTree instance
-;  ECX = rootNode's logical offset in sectors
-;
-lookUpBTree:
-	mov		cx, BTree_size							; allocate memory with BTree_size
-	call	malloc									; BP = start address of allocated memory.
-	mov		[bp + BTree.fileID], al					; save fileFileID
-	mov		edx, [di]								; first extent of current file
-	call	blockToSector							; ECX = converted to sector unit
-	mov		al, 1									; 1 sector is enough for
-	xor		edx, edx								; reading current file's header.
-	lea		dx, [bp + BTree.BTHeaderBuffer]			; load into BTreeHeaderBuffer
-	call	readLBA									; read
-	mov		ax, [bp + BTree.BTHeaderBuffer + BTNodeDescriptor_size + BTHeaderRec.nodeSize]
-	xchg	ah, al									; convert to little-endian
-	mov		[bp + BTree.nodeSize], ax				; save nodeSize
-
-	;
-	; Always start the lookup process with the root node.
-	;
-	mov		edx, [bp + BTree.BTHeaderBuffer + BTNodeDescriptor_size + BTHeaderRec.rootNode]
-
-.readNode:
-	;
-	; Converting nodeID to sector unit
-	;
-	mov		ax, [bp + BTree.nodeSize]
-	shr		ax, 9									; convert nodeSize to sectors
-	mov		bx, ax									; BX = read sector count
-	cwde
-	bswap	edx										; convert node ID to little-endian
-	mul		edx										; multiply with nodeSize converted to sector unit
-	mov		ecx, eax								; ECX = file offset in BTree
-
-	mov		eax, [bp + BTree.fileID]
-	lea		edx, [bp + BTree.nodeBuffer]
-	call	readExtent
-
-	;
-	; AX = lowerBound = 0
-	;
-	xor		ax, ax
-
-	;
-	; BX = upperBound = numRecords - 1
-	;
-	mov		bx, [bp + BTree.nodeBuffer + BTNodeDescriptor.numRecords]
-	xchg	bh, bl
-	dec		bx
-	
-.bsearch:
-	cmp		ax, bx
-	ja		.checkResult							; jump if lowerBound > upperBound
-	
-	mov		cx, ax
-	add		cx, bx
-	shr		cx, 1									; test index = (lowerBound + upperBound / 2)
-
-	call	getBTreeRecord
-
-%if UNUSED
-	pushad
-	jl		.csearchLessThanTrial
-	jg		.csearchGreaterThanTrial
-	PrintChar('=')
-	jmp		.csearchCont
-.csearchGreaterThanTrial:
-	PrintChar('>')
-	jmp		.csearchCont
-.csearchLessThanTrial:
-	PrintChar('<')
-.csearchCont:
-	popad
-%endif ; UNUSED
-	
-.adjustBounds:
-	je		.checkResult
-	jl		.searchLessThanTrial
-	jg		.searchGreaterThanTrial
-	jmp		.bsearch	
-
-.searchLessThanTrial:
-	mov		bx, cx
-	dec		bx										; upperBound = index - 1
-	jmp		.bsearch
-
-.searchGreaterThanTrial:
-	mov		ax, cx
-	inc		ax										; lowerBound = index + 1
-	jmp		.bsearch
-	
-.checkResult:
-	cmp		BYTE [bp + BTree.searchResult], 0
-	jge		.foundKey
-
-	mov		cx, bx
-	call	getBTreeRecord
-
-.foundKey:
-	cmp		BYTE [bp + BTree.nodeBuffer + BTNodeDescriptor.kind], kBTIndexNode
-	jne		.exit
-
-	lea		bx, [bp + BTree.recordDataPtr]
-	mov		bx, [bx]
-	mov		edx, [bx]
-	jmp		.readNode
-	
-.exit:
-	cmp		BYTE [bp + BTree.searchResult], 0
+PrintNibble:
+	and     al, 0x0f
+	add     al, '0'
+	cmp     al, '9'
+	jna     .print_ascii
+	add     al, 'A' - '9' - 1
+.print_ascii:
+	call    PrintChar
 	ret
 
 ;--------------------------------------------------------------------------
-; getBTreeRecord - read and compare BTree record
+; Print a ASCII character.
 ;
 ; Arguments:
-;   CX = record index
-;   SI = address of search key
+;   AL = ASCII character.
 ;
-; Returns:
-;   [BTree.searchResult] = result of key compare
-;   [BTree.recordDataPtr] = address of record data
-;
-getBTreeRecord:
+PrintChar:
 	pushad
-	push	si									; save SI
-	lea		di, [bp + BTree.nodeBuffer]			; DI = start of nodeBuffer
-	push	di									; use later
-	mov		ax, [bp + BTree.nodeSize]			; get nodeSize
-	add		di, ax								; DI = beyond nodeBuffer
-	inc		cx									; increment index
-	shl		cx, 1								; * 2
-	sub		di, cx								; DI = pointer to record
-	mov		ax, [di]							; offset to record
-	xchg	ah, al								; convert to little-endian
-	pop		di									; start of nodeBuffer
-	add		di, ax								; DI = address of record key
-	mov		si, di								; save to SI
-	mov		ax, [di]							; keyLength
-	xchg	ah, al								; convert to little-endian
-	inc		ax									; suppress keySize (2 bytes)
-	inc		ax									;
-	add		di, ax								; DI = address of record data
-	mov		[bp + BTree.recordDataPtr], di		; save address of record data
-	lea		di, [bp + BTree.trialKey]
-	push	di									; save address of trialKey
-	lodsw										; suppress keySize (2 bytes)
-	;
-	; Don't need to compare as DWORD since all reserved CNIDs fits to a single byte
-	;
-	cmp		BYTE [bp + BTree.fileID], kHFSCatalogFileID
-	je		.prepareTrialCatalogKey
-
-.prepareTrialExtentKey:
-	mov		bx, compareHFSPlusExtentKeys
-	movsw										; copy forkType + pad
-	mov		cx, 2								; copy fileID + startBlock
-
-.extentLoop:
-	lodsd
-	bswap	eax									; convert to little-endian
-	stosd
-	loop	.extentLoop
-	jmp		.exit
-
-.prepareTrialCatalogKey:
-	mov		bx, compareHFSPlusCatalogKeys
-	lodsd
-	bswap	eax									; convert ParentID to little-endian
-	stosd
-	call	ConvertHFSUniStr255ToLE				; convert nodeName to little-endian
-
-.exit:
-	pop		di									; restore address of trialKey
-
-%if UNUSED	
-;
-; Print catalog trial key
-;
-	pushad
-	mov		si, di
-	lodsd
-	PrintChar('k')
-	PrintHex()
-	lodsw
-	cmp		ax, 0
-	je		.printExit
-	mov		cx, ax
-.printLoop:
-	lodsw
-	call	print_char
-	loop	.printLoop  
-.printExit:
-	popad
-;
-;
-;
-%endif ; UNUSED
-	
-%if UNUSED	
-;
-; Print extent trial key
-;
-	pushad
-	PrintChar('k')
-	mov		si, di
-	xor		eax, eax
-	lodsw
-	PrintHex()
-	lodsd
-	PrintHex()
-	lodsd
-	PrintHex()
-	popad
-;
-;
-;
-%endif ; UNUSED
-
-	pop		si									; restore SI
-	call	bx									; call key compare proc
-	popad
-	ret 
-
-;--------------------------------------------------------------------------
-; readExtent - read extents from a HFS+ file (multiple extent support)
-;
-; Arguments:
-;  EAX = Catalog File ID
-;  	BX = read size in sectors
-;  ECX = file offset in sectors
-;  EDX = address of read buffer
-;   DI = address of HFSPlusForkData.extents
-;
-readExtent:
-	pushad
-	;
-	; Save Catalog File ID as part of a search HFSPlusExtentKey
-	; for a possible Extents Overflow lookup.
-	;
-	mov		[bp + BTree.searchExtentKey + HFSPlusExtentKey.fileID], eax
-	mov		[bp + BTree.readBufferPtr], edx
-	mov		ax, bx
-	cwde
-	mov		[bp + BTree.readSize], eax
-	mov		ebx, ecx							; EBX = file offset
-	xor		eax, eax
-	mov		[bp + BTree.currentExtentOffs], eax
-
-.beginExtentBlock:
-	mov		BYTE [bp + BTree.extentCount], 0
-
-.extentSearch:
-	cmp		BYTE [bp + BTree.extentCount], kHFSPlusExtentDensity
-	jb		.continue
-
-.getNextExtentBlock:
-	push	ebx
-	mov		eax, [bp + BTree.currentExtentOffs]
-
-	;
-	; Converting sector unit to HFS+ allocation block unit.
-	;
-	xor		edx, edx
-	div		DWORD [gBlockSize]				; divide with blockSize
-
-	;
-	; Preparing searchExtentKey's startBlock field.
-	;
-	mov		[bp + BTree.searchExtentKey + HFSPlusExtentKey.startBlock], eax
-
-	mov		al, kHFSExtentsFileID
-	lea		si, [bp + BTree.searchExtentKey]
-	lea		di, [kHFSPlusBuffer + HFSPlusVolumeHeader.extentsFile + HFSPlusForkData.extents]
-	call	lookUpBTree
-	jnz		NEAR .exit
-
-	;
-	; BP points to the new workspace allocated by lookUpBTree.
-	;
-	lea		di, [bp + BTree.recordDataPtr]
-	mov		di, [di]
-
-	;
-	; Switch back to the previous workspace.
-	;
-	lea		bp, [gMallocPtr]
-	mov		bp, [bp]
-	mov		[gMallocPtr], bp
-
-	pop		ebx
-	jmp		.beginExtentBlock
-	
-.continue:
-	mov		edx, [di + HFSPlusExtentDescriptor.blockCount]
-	call	blockToSector								; ECX = converted current extent's blockCount to sectors
-	mov		eax, [bp + BTree.currentExtentOffs]			; EAX = current extent's start offset (sector)
-	mov		edx, eax
-	add		edx, ecx									; EDX = next extent's start offset (sector)
-	cmp		ebx, edx
-	mov		[bp + BTree.currentExtentOffs], edx			; set currentExtentOffs as the next extent's start offset
-	jae		.nextExtent									; jump to next extent if file offset > next extent's start offset
-
-.foundExtent:
-	mov		edx, ebx
-	sub		edx, eax									; EDX = relative offset within current extent
-	mov		eax, edx									; will be used below to determine read size
-	mov		esi, [bp + BTree.readSize]					; ESI = remaining sectors to be read
-	add		edx, esi
-	cmp		edx, ecx									; test if relative offset + readSize fits to this extent
-	jbe		.read										; read all remaining sectors from this extent
-
-.splitRead:
-	sub		ecx, eax									; read amount of sectors beginning at relative offset
-	mov		esi, ecx									; of current extent up to the end of current extent
-
-.read:
-	mov		edx, [di + HFSPlusExtentDescriptor.startBlock]
-	call	blockToSector								; ECX = converted to sectors
-	add		ecx, eax									; file offset converted to sectors
-	
-	push	si
-	mov		ax, si
-	mov		edx, [bp + BTree.readBufferPtr]
-	call	readSectors
-	pop		si
-	
-	add		ebx, esi
-	mov		ax, si
-	cwde
-	shl		ax, 9										; convert SI (read sector count) to byte unit
-	add		[bp + BTree.readBufferPtr], eax
-	sub		[bp + BTree.readSize], esi
-	
-	jz		.exit
-
-.nextExtent:
-	add		di, kHFSPlusExtentDensity
-	inc		BYTE [bp + BTree.extentCount]
-	jmp		.extentSearch
-
-.exit:
+	mov     bx, 1                   		; BH=0, BL=1 (blue)
+	mov     ah, 0x0e                		; bios INT 10, Function 0xE
+	int     0x10                    		; display byte in tty mode
 	popad
 	ret
 
-;--------------------------------------------------------------------------
-; Convert big-endian HFSPlus allocation block to sector unit
-;
-; Arguments:
-;   EDX = allocation block
-;
-; Returns:
-;   ECX = allocation block converted to sector unit
-;
-; Clobber list:
-;   EDX
-;
-blockToSector:
-	push	eax
-	mov		eax, [gBlockSize]
-	bswap	edx								; convert allocation block to little-endian
-	mul		edx				 				; multiply with block number
-	mov		ecx, eax						; result in EAX
-	pop		eax
-	ret
+%endif ; DEBUG
 
-%if UNUSED
+%if DEBUG
 
 ;--------------------------------------------------------------------------
-; Convert sector unit to HFSPlus allocation block unit
-;
-; Arguments:
-;   EDX = sector
-;
-; Returns:
-;   ECX = converted to allocation block unit
-;
-; Clobber list:
-;   EDX
-;
-sectorToBlock:
-	push	eax
-	mov		eax, edx
-	xor		edx, edx
-	div		DWORD [gBlockSize]				; divide with blockSize
-	mov		ecx, eax						; result in EAX
-	pop		eax
-	ret
-
-%endif ; UNUSED
-
-%if UNUSED
-
-;--------------------------------------------------------------------------
-; Convert big-endian BTree node ID to sector unit
-;
-; Arguments:
-;   EDX = node ID
-;
-; Returns:
-;   ECX = node ID converted to sector unit
-;
-; Clobber list:
-;   EDX
-;
-nodeToSector:
-	push	eax
-	mov		ax, [bp + BTree.nodeSize]
-	shr		ax, 9							; convert nodeSize to sectors
-	cwde
-	bswap	edx								; convert node ID to little-endian
-	mul		edx								; multiply with node ID
-	mov		ecx, eax						; result in EAX
-	pop		eax
-	ret
-
-%endif ; UNUSED
-
-;--------------------------------------------------------------------------
-; Static data.
-;
-
-%if VERBOSE
-log_title_str		db		CR, LF, 'boot1: ', NULL
-error_str			db		'error', NULL
-%endif
-
-searchCatalogKey	dd		kHFSRootFolderID
-					dw		searchCatKeyNameLen
-searchCatKeyName	dw		'b', 'o', 'o', 't'			; must be lower case
-searchCatKeyNameLen	EQU		($ - searchCatKeyName) / 2
-
-;--------------------------------------------------------------------------
-; Pad the rest of the 512 byte sized sector with zeroes. The last
-; two bytes is the mandatory boot sector signature.
-;
-pad_sector_1:
-	times			1022-($-$$) db 0
-	dw				kBootSignature
-
-;
-; Local BTree variables
-;
-					struc			BTree
-.mallocLink			resw			1						; pointer to previously allocated memory block
-.fileID				resd			1						; will use as BYTE
-.nodeSize			resd			1						; will use as WORD
-.searchExtentKey	resb			HFSPlusExtentKey_size
-.searchResult		resb			1
-.trialKey			resb			kBTMaxRecordLength
-.recordDataPtr		resw			1
-.readBufferPtr		resd			1
-.currentExtentOffs	resd			1
-.readSize			resd			1
-.extentCount		resb			1
-	ALIGNB			2
-.BTHeaderBuffer		resb			kSectorBytes
-.nodeBuffer			resb			maxNodeSize
-					endstruc
-
-;
-; Global variables
-;
-
-	ABSOLUTE		kHFSPlusBuffer + HFSPlusVolumeHeader_size
-
-gPartLBA			resd	1
-gBIOSDriveNumber	resw	1
-gBlockSize			resd	1
-gMallocPtr			resw	1
-
-; END
